ea833e397f8be3b695505e8c5a07488dd9493978
Arne <arne.cornils@gmx.de>
2026-03-15 13:06:23 +0100

    renamed app an package

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.pfennig50.app',
-  appName: '50Pfennig',
+  appId: 'com.arco.sharli',
+  appName: 'Sharli',
 
   // Vite build output directory
   webDir: 'dist',
